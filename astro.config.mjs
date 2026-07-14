b36730b35d929de25659c885e2dbf4e9a087647e
ilhamelin <benjaigancioreyes56@gmail.com>
2026-07-14 07:55:17 -0400

    feat: configure site settings and add GitHub Actions deployment workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://ilhamelin.github.io',
+  base: '/astro-multistore',
   vite: {
     plugins: [tailwindcss()]
   },
